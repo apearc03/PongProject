48fd44f6b554f0e601e95c56adca3eeb44d86b0f
Alex <arpearce93@gmail.com>
2017-11-02 11:02:11 +0000

    LibGDX project generated

---
+++ core/src/pongproject/game/pong.java
@@ -0,0 +1,23 @@
+package pongproject.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+
+public class pong extends ApplicationAdapter {
+
+	
+	@Override
+	public void create () {
+		
+	}
+
+	@Override
+	public void render () {
+		
+	}
+	
+	@Override
+	public void dispose () {
+		
+	}
+}
